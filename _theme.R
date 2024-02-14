7861527caa0a9f84a9fc41d7e38ab4cda1d3498b
Kristy Ou <kou@seneca.embl.de>
2024-02-14 21:34:49 +0100

    update numbat

--- _theme.R
+++ _theme.R
@@ -1,13 +1,21 @@
 #### ---- PLOT FUNCTIONS ---- ####
 
 # standard theme for umap
-theme_umap = function(){
+theme_umap = function(...){
   theme_bw() +
     theme(
+      ...,
       aspect.ratio = 3/3,
       panel.grid = element_blank(),
       strip.background = element_blank(),
       axis.ticks = element_blank(),
       axis.text = element_blank()
-    )
+    ) 
+}
+
+fix_legend = function(...){
+  guides(
+    color = guide_legend(override.aes = list(size = 2)),
+    nrow = 2
+  )
 }
